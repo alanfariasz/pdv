fc08c08b77531d63a53402ee538ea2b4a19f4a73
Alan Farias <93013258+alanfariasz@users.noreply.github.com>
2023-11-30 13:34:51 -0300

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder',
+    pathMatch: 'full',
+  },
+  {
+    path: 'folder',
+    loadComponent: () =>
+      import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'usuarios',
+    loadComponent: () => import('./usuarios/usuarios.page').then( m => m.UsuariosPage)
+  },
+];
---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,18 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+})
+export class FolderPage implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
